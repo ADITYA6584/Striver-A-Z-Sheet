57deb9073c6a4e6cd3926cc55070f0f26d057ac0
ADITYA6584 <akskdk678@gmail.com>
2024-10-08 08:20:57 +0530

    Peak Element

--- Step-4/Lec1/PeakElement.cpp
+++ Step-4/Lec1/PeakElement.cpp
@@ -35,4 +35,39 @@ public:
 
 /***********************************************************************************************************/
 
+//Optimal Approach O(logN)
 //Approach -> 2  Using Binary Search
+
+//Key Point is -> Carefully handle edge cases like the starting and the end
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) 
+    {
+        //edgecase
+        if(nums.size()==1) return 0;
+        //For starting edgeCase
+        if(nums[0]>nums[1]) return 0;
+        //For end case 
+        else if(nums[nums.size()-1]>nums[nums.size()-2]) return nums.size()-1;
+        int s=1;
+        int e = nums.size()-2;
+        int mid = s+(e-s)/2;
+        while(s<=e)
+        {
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1] )
+            {
+                return mid;
+            }
+            if(nums[mid]>nums[mid-1] && nums[mid]<nums[mid+1])
+            {
+                s = mid+1;
+            }
+            else
+            {
+                e = mid-1;
+            }
+            mid = s+(e-s)/2;
+        }
+        return nums.size()-2;
+    }
+};
